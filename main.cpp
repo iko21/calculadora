57a17a92861cca99e1b85d2e66d9df1d5f677533
mac21 <mac21@mac21-Dell.(none)>
2014-01-20 12:21:54 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int opc;
+
+    cout << "CASIO" << endl;
+    cout << "1. Suma" << endl;
+    cout << "2. Resta" << endl;
+    cout << "3. Multiplicacion" << endl;
+    cout << "4. Division" << endl;
+    cout <<"Elija una opcion" << endl;
+    cin >> opc;
+    switch(opc){
+
+
+
+
+
+    }
+
+    return 0;
+}
